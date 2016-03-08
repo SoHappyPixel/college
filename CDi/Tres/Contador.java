bb8fc6b0c5111b9f94f038b3b029de1ec26799ed
Daniel Cábalas <danicamba@me.com>
2016-03-08 17:44:54 +0100

    cdi pract 3, ap reindentado

---
+++ CDi/Tres/Contador.java
@@ -0,0 +1,16 @@
+package Tres;
+
+public class Contador
+{
+    private int res = 0;
+    
+    public int Incrementar(int n)
+    {
+        for(int i=0; i<n; i++) res++;
+
+        return res;
+    }
+}
+
+
+
