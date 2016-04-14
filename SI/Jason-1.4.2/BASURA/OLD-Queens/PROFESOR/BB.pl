ecfcc05cd078b50c5ccec10e2f6f37b2034082bb
Daniel Cábalas <danicamba@me.com>
2016-04-14 18:12:43 +0200

    fix SI

--- SI/Jason-1.4.2/BASURA/OLD-Queens/PROFESOR/BB.pl
+++
@@ -1,4 +0,0 @@
-bb(Size,Size,Size,[[Size,Size]]).
-bb(It,It,Size,[[It,It]|L]):- N_val is It+1, bb(N_val,It,Size,L).
-bb(Size,It,Size,[[Size,It]|L]):- N_it is It+1, bb(0,N_it,Size,L).
-bb(Val,It,Size,[[Val,It]|L]):- N_val is Val+1, bb(N_val,It,Size,L).
