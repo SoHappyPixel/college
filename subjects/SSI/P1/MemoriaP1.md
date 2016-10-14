cb02a31dadb3fa739b060af9e66eecac7110f5b0
Daniel Cábalas <danicamba@me.com>
2016-10-14 20:56:43 +0200

    cosis de SSI y ABP

---
+++ subjects/SSI/P1/MemoriaP1.md
@@ -0,0 +1,81 @@
+#SSI - CompostelaVirtual - Práctica1.
+
+##Enunciado principal.
+- Los **(P)eregrinos** podrán generar su *Compostela Virtual*.
+- Será sellada por los **(A)lbergues** por donde pase.
+- La **(O)ficina** podrá validar la *Compostela Virtual*.
+  - Extraer los datos del peregrino.
+  - Validar los *sellos* de los albergues.
+
+###Aclaraciones.
+- Los 3 actores tendrán clave pública y privada.
+- Pre y Post encriptado los datos serán strings JSON.
+- Se omiten los mecanismos de distribución de clave pública.
+- Si en **O** falla alguna comprobación, se indicarán los sellos o información fallida; si todo es correcto se mostrará la totalidad de la *Compostela*.
+
+###Accesos.
+- **P** <- *Ku* de **O**.
+- **A** <- *Ku* de **O** y **P** (al sellar).
+- **O** <- *Ku* de **A's** y **P** (al validar).
+
+###Requisitos.
+- Confidencialidad datos de **P**, sólo **O** podrá leerlos.
+  - Confidencialidad datos de **A**, sólo legibles por **O**.
+- Que **O** tenga la seguridad de que el **P** que presenta la *Compostela* sea el **P** dueño de la *Compostela*.
+- Asesgurar que no haya información modificada en el paquete.
+- Evitar que **P** o **A** puedan repudiar la info añadida por ellos.
+- Evitar que **O** pueda realizar cambios en el contenido de la *Compostela*.
+- Mecanismo de *Sellado de tiempo* para **A**.
+- *Minimo coste computacional* y uso de *asiméticos*.
+
+###Desarrollo.
+- **GenerarClaves <ID>** (Ya implementado).
+  - Usado al inicio por los 3 actores.
+  - Genera: ID.publica, ID.privada.
+- **GenerarCompostela <Nombre_Paquete> <Ficheros_K>**
+  - Usado por **P**. (<Ficheros_K> depende de la estrategia)
+  - Solicita (como Strings): *Nombre, DNI, domicilio, fecha y lugar de creación, motivaciones.*
+  - Genera <Nombre_Paquete>.paquete, empaquetando los datos solicitados (en un bloque dentro del paquete).
+- **SellarCompostela <P_Paquete> <ID_Albergue> <Ficheros_Ku>**
+  - Usado por **A**.
+  - Los bloques se bautizarán como <ID_Albergue_NombreBloque>
+  - Al <P_Paquete> le añade el/los bloques del <ID_Albergue>.
+    - Los bloques contienen la siguiente información (como String): *Nombre completo del albergue, fecha y lugar de creación, incidencias.*
+  - Devuelve la *Compostela* (<P_Paquete>) con el nuevo sello.
+- **DesempaquetarCompostela <P_Paquete> <Num_Albergues> <ID_Albergue_N> <KU_Albergue_N> <Otros_Ficheros_Ku>**
+  - Usado por **O**.
+  - Verifica que la *Compostela* pertenezca a quien la presenta.
+  - Verifica que ningun dato haya sido modificiado.
+  - Verificará los sellos.
+    - Si todos son correctos, se mostrarán por pantalla.
+    - Si hay errores, se muestran los sello de conflicto.
+
+
+
+## Memoria de la práctica.
+
+### Descripción breve de la práctica.
+
+
+
+### Descripción y justificación de las estrategias criptográficas empleadas para asegurar los requisitos básicos exigidos, junto con los pasos que se siguen en el empaquetado, sellado y desempaquetado.
+
+z
+
+### Descripción del formato/estructura del "paquete" resultante.
+
+
+
+### Descripción breve de la implementación: clases y métodos más importantes.
+
+
+
+### Instrucciones de compilación y ejemplos de uso.
+
+
+
+### Breve comentario sobre las simplificaciones realizadas (apartado 2.1) y sus consecuencias en una aplicación real.
+
+
+
+### Resultados obtenidos y conclusiones.
---
+++ subjects/SSI/P1/Src/GenerarCompostela.java
@@ -0,0 +1,112 @@
+import java.io.*;
+import java.util.*;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+
+import java.security.*;
+import java.security.spec.*;
+
+import javax.crypto.*;
+import javax.crypto.spec.*;
+import javax.crypto.interfaces.*;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+
+public class GenerarCompostela {
+
+    public static void main(String[] args) throws Exception {
+
+        // Pedir datos.
+        Map<String, String> json_datos = new HashMap<String, String>();
+        Scanner scan = new Scanner (System.in);
+
+        System.out.print("Nombre: ");
+        String nombre = scan.nextLine ();
+        json_datos.put("nombre",nombre);
+
+        System.out.print("DNI: ");
+        String dni = scan.nextLine ();
+        json_datos.put("dni",dni);
+
+        System.out.print("Domicilio: ");
+        String domicilio = scan.nextLine ();
+        json_datos.put("domicilio",domicilio);
+
+        System.out.print("Fecha: ");
+        String fecha = scan.nextLine ();
+        json_datos.put("fecha",fecha);
+
+        System.out.print("Lugar: ");
+        String lugar = scan.nextLine ();
+        json_datos.put("lugar",lugar);
+
+        System.out.print("Motivacion: ");
+        String motivacion = scan.nextLine ();
+        json_datos.put("motivacion",motivacion);
+
+        scan.close();
+
+
+        // Covertir datos a JSON.
+        String json_peregrino = JSONUtils.map2json( json_datos );
+
+        // Convertir
+        byte[] bytes_peregrino = json_peregrino.getBytes();
+
+
+        //--------------------------------------------------------
+
+        // BC como provider.
+        Security.addProvider(new BouncyCastleProvider());
+
+        // SHA para el hash.
+        MessageDigest hasher = MessageDigest.getInstance("SHA");
+		hasher.update(bytes_peregrino, 0, bytes_peregrino.length);
+        byte[] hash_peregrino = hasher.digest();
+
+
+        // Leer las claves ...
+        FileInputStream file;
+        KeyFactory keyFactoryRSA = KeyFactory.getInstance("RSA", "BC");
+
+        // Clave privada peregrino.
+        Path path_kr_p = Paths.get(args[1]);
+        byte[] kr_p = Files.readAllBytes(path_kr_p);
+        PKCS8EncodedKeySpec kr_p_spec = new PKCS8EncodedKeySpec( kr_p );
+        PrivateKey kr_p_rsa = keyFactoryRSA.generatePrivate(kr_p_spec);
+
+        // Clave publica oficina.
+        Path path_ku_o = Paths.get(args[2]);
+        byte[] ku_o = Files.readAllBytes(path_ku_o);
+        X509EncodedKeySpec ku_o_spec = new X509EncodedKeySpec( ku_o );
+        PublicKey ku_o_rsa = keyFactoryRSA.generatePublic(ku_o_spec);
+
+
+        // Cifrado con RSA del hash.
+        Cipher rsa = Cipher.getInstance("RSA", "BC");
+        rsa.init(Cipher.ENCRYPT_MODE, kr_p_rsa);
+        byte[] rsa_hash_peregrino = rsa.doFinal(hash_peregrino);
+
+
+        // Crea el bloque de la Firma.
+        Bloque firma_p = new Bloque("firma_p",rsa_hash_peregrino);
+
+        //--------------------------------------------------------
+
+
+        // // Crear bloque con los datos JSON.
+        // Bloque datos_p = new Bloque("datos_p",);
+        // Bloque des_p = new Bloque("des_p",);
+        //
+        //
+        // // Crear paquete (Compostela).
+        // Paquete compostela = new Paquete();
+
+        // Añadir bloque a la Compostela.
+
+
+    }
+}
