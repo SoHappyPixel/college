3d3465f59b16aa9448ee2d62d905c94264cbe842
Gophre <danicamba@me.com>
2016-10-31 03:41:14 +0100

    django project update

--- SN/Django/SocksStore/Store/views.py
+++ SN/Django/SocksStore/Store/views.py
@@ -1,27 +1,34 @@
-from django.shortcuts import render, HttpResponseRedirect
-
+from .models import Product
 from .forms import ProductForm
 from .forms import CustomerForm
+from django.shortcuts import render, HttpResponseRedirect
 
-from .models import Product
+def check_form( form, redirect_path ):
+    if form.is_valid():
+        instance = form.save(commit = False)
+        print instance
+        instance.save()
+        return HttpResponseRedirect( redirect_path )
 
 
+# home is the Index view.
 def home(request):
     welcome = 'A new reality for your feet !'
     context = { 'welcome': welcome }
     return render(request,'Home.html',context)
 
 
+# CUSTOMER VIEWS.
+
+# ... profileCustomer let your clients view themselves profile.
+
+# ... newCustomer let you add clients (or that them register) to your site.
 def newCustomer(request):
     form = CustomerForm( request.POST or None )
-
-    if form.is_valid():
-        instance = form.save(commit = False)
-        print instance
-        instance.save()
-        return HttpResponseRedirect( '/newCustomer' )
+    check_form( form, '/newCustomer' )
 
     title = 'Join now !!!'
+
     context = {
         'title':title,
         'form':form,
@@ -30,32 +37,31 @@ def newCustomer(request):
     return render(request,'newCustomer.html',context)
 
 
+# PRODUCT VIEWS.
+
+# ... newProduct let you add products to your site.
 def newProduct(request):
     form = ProductForm( request.POST or None )
-
-    if form.is_valid():
-        instance = form.save(commit = False)
-        print instance
-        instance.save()
-        return HttpResponseRedirect( '/newProduct' )
+    check_form( form, '/newProduct' )
 
     title = 'Add item !'
-    context={
-        'title':title,
+
+    context = {
         'form':form,
+        'title':title,
     }
 
-    return render(request,'NewProduct.html',context)
+    return render( request, 'NewProduct.html', context )
 
-
+# ... listProducts let you visualize all the prodcucts on your catalog.
 def listProducts(request):
-    querySet = Product.objects.all()
+    query_set = Product.objects.all()
 
     title = 'Product List ...'
 
     context = {
-        'querySet' : querySet,
+        'query_set' : query_set,
         'title' : title,
     }
 
-    return render(request,'ListProducts.html',context)
+    return render( request, 'ListProducts.html', context )
