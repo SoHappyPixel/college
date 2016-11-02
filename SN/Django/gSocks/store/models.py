9e4be60958a4518ce905513e6dab14ed5475d484
Gophre <danicamba@me.com>
2016-11-02 04:32:57 +0100

    huuuuuuuge django project update

--- SN/Django/gSocks/store/models.py
+++ SN/Django/gSocks/store/models.py
@@ -1,16 +1,42 @@
+from django.contrib.auth.models import User
 from django.urls import reverse
 from django.db import models
 
+TEXT_SIZE = 150
+
+THICKNESS_CHOICES = (
+    ('0', 'Thick'),
+    ('1', 'Thin'),
+)
+
+HEIGHT_CHOICES = (
+    ('0', 'High'),
+    ('1', 'Mid'),
+    ('2', 'Low'),
+)
+
 class Product(models.Model):
-    brand = models.CharField(max_length=120)
-    thickness = models.IntegerField()
-    color = models.CharField(max_length=120)
-    height = models.IntegerField()
-    cost = models.DecimalField(max_digits=3, decimal_places=2)
-    printed = models.CharField(max_length=120)
+    name = models.CharField(max_length = TEXT_SIZE)
+    brand = models.CharField(max_length = TEXT_SIZE)
+    color = models.CharField(max_length = TEXT_SIZE)
+    printed = models.CharField(max_length = TEXT_SIZE)
+    height = models.CharField(max_length=1, choices=HEIGHT_CHOICES)
+    thickness = models.CharField(max_length=1, choices=THICKNESS_CHOICES)
+    cost = models.DecimalField(max_digits = 3, decimal_places = 2)
 
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=True, auto_now=False)
 
     def get_absolute_url(self):
-        return reverse('product-detail', kwargs={'pk': self.pk})
+        return reverse('product_detail', kwargs={'pk': self.pk})
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, related_name='+')
+    item = models.ForeignKey(Product)
+
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def get_absolute_url(self):
+        return reverse('cart_view', kwargs={'pk': self.pk})
--- SN/Django/gSocks/store/urls.py
+++ SN/Django/gSocks/store/urls.py
@@ -1,12 +1,19 @@
-from django.conf.urls import include, url
 from . import views
+from django.conf.urls import include, url
+from django.contrib.auth.views import login
 
 app_name = 'store'
+
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^product/$', views.ProductList.as_view(), name='product-list'),
-    url(r'^product/(?P<pk>[0-9]+)/$', views.ProductDetail.as_view(), name='product-detail'),
-    url(r'^product/add/$', views.ProductCreate.as_view(), name='product-add'),
-    url(r'^product/(?P<pk>[0-9]+)/update/$', views.ProductUpdate.as_view(), name='product-update'),
-    url(r'^product/(?P<pk>[0-9]+)/delete/$', views.ProductDelete.as_view(), name='product-delete'),
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', views.logout.as_view(), name='logout'),
+    url(r'^register/$', views.UserCreate.as_view(), name='user_add'),
+
+    url(r'^product/$', views.ProductList.as_view(), name='product_list'),
+    url(r'^product/add/$', views.ProductCreate.as_view(), name='product_add'),
+    url(r'^product/(?P<pk>[0-9]+)/$', views.ProductDetail.as_view(), name='product_detail'),
+    url(r'^product/(?P<pk>[0-9]+)/update/$', views.ProductUpdate.as_view(), name='product_update'),
+    url(r'^product/(?P<pk>[0-9]+)/delete/$', views.ProductDelete.as_view(), name='product_delete'),
+
 ]
